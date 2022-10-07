177ad214eff4da63b6903f41731fed97b84c52b4
Tatiana <kravchenkodevelop@gmail.com>
2022-10-07 20:15:32 +0300

    task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int a;
+try
+{
+     a = int.Parse(Console.ReadLine());
+    a = a / 10;
+    a= a % 10;
+    //Math.DivRem(a, 10, out a);
+    Console.WriteLine(a.ToString());
+}
+catch (Exception x ) { Console.WriteLine(x.Message); }
+
+Console.ReadKey();
