18febaa9b0ae232a696b6e62b1f2b9ba72a1fc61
Crafty-Raccoon <kostoev-23@yandex.ru>
2022-10-10 19:41:01 +0300

    task3 compete

---
+++ task3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число");
+ConsoleKeyInfo inpKey = new
+ConsoleKeyInfo();
+int k = 0;
+bool flag = true;
+int res = 0;
+while (flag)
+{
+    inpKey = Console.ReadKey();
+    if (inpKey.Key != ConsoleKey.Enter)
+    {
+        k++;
+        if (k == 3) ;
+        {
+            res = inpKey.KeyChar;
+            res = int.Parse(Char.ConvertFromUtf32(res));
+        }
+    }
+    else { flag = !flag; }
+}
+Console.WriteLine("");
+if (k < 3)
+{ Console.WriteLine("Оно не трехзначное"); }
+else
+{ Console.WriteLine(res); }
+Console.ReadKey();
