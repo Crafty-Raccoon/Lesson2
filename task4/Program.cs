5c94fc375c9952a26359f2aa180d6ce8a45f2d0a
Crafty-Raccoon <Kostoev-23@yandex.ru>
2022-10-11 18:01:50 +0300

    task4 compete

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int day;
+try
+{
+    day = int.Parse(Console.ReadLine());
+    if (day > 0 && day < 8)
+    {
+        if (day < 6) { Console.WriteLine("Рабочий день"); }
+        else Console.WriteLine("Выходной");
+    }
+    else Console.WriteLine("Не день недели");
+}
+catch (Exception x) { Console.WriteLine(x.Message); }
+
+Console.ReadKey();
